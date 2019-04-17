933a1821d0dbe4b83976891fe92e81c472cf3504
marcgragen <39063858+marcgragen@users.noreply.github.com>
2019-04-17 10:28:59 +0200

    Baralla funciona provat amb enters, inicialitzada a null i es va substituint per la fitxa cada vegada que n'entra una

--- src/Baralla.java
+++ src/Baralla.java
@@ -1,2 +1,83 @@
+import java.util.Vector;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Baralla {
+
+    private int nFitxes;
+    private Vector<Fitxa> vectorFitxes;
+    private boolean buit;
+    private int sentinella;
+
+    Baralla(int nFit)
+    {
+        nFitxes = nFit;
+        vectorFitxes = new Vector(nFitxes);
+        for (int i=0; i<nFitxes; i++)
+        {
+            vectorFitxes.add(i, null);
+        }
+        buit = true;
+        sentinella = 0;
+    }
+
+    ///Pre: ---
+    ///Post: Agafa una fitxa de la baralla de fitxes i la retorna
+    public Fitxa agafarFitxa()
+    {
+        Fitxa f = (Fitxa) vectorFitxes.get(sentinella);
+        vectorFitxes.set(sentinella, null);
+        actualitzarSentinella();
+        return f;
+    }
+
+    ///Pre: f != NULL
+    ///Post: S'afageix la fitxa f de forma ALEATÒRIA
+    public void afegirFitxa(Fitxa f)
+    {
+        int aleatori = ThreadLocalRandom.current().nextInt(0, nFitxes-1);
+        while (vectorFitxes.get(aleatori) != null)
+        {
+            aleatori++;
+            if (aleatori == nFitxes)
+            {
+                aleatori = 0;
+            }
+        }
+
+        buit = false;
+        vectorFitxes.set(aleatori, f);
+    }
+
+    ///Pre: ---
+    ///Post: Retorna si la baralla s'ha quedat sense fitxes
+    public boolean esBuida()
+    {
+        return buit;
+    }
+
+    ///Pre: ---
+    //Post: Coloca el sentinella a la posició més petita possible
+    private void actualitzarSentinella()
+    {
+        /*while (vectorFitxes.get(sentinella) == 0)
+        {
+            sentinella++;
+            if (sentinella == nFitxes-1)
+            {
+                sentinella = 0;
+            }
+        }*/
+        sentinella++;
+    }
+
+    ///Pre: ---
+    //Post: Mostra les fitxes de la baralla separades per un espai
+    public void mostrar()
+    {
+        for (Fitxa i : vectorFitxes) {
+            System.out.print(i);
+            System.out.print(" ");
+        }
+    }
+
 }
--- src/Controlable.java
+++ src/Controlable.java
@@ -1,2 +1,13 @@
+import java.util.List;
+
 public class Controlable extends Jugador {
+    @Override
+    void PosaFitxaAlTauler(List<Posicio> pos) {
+
+    }
+
+    @Override
+    void PosarSeguidor(Regio r) {
+
+    }
 }
