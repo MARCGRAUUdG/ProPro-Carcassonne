38383eae6c11bc9516e64afbf1a46677a8f2b3c4
Soap1080 <zjavi.1080@gmail.com>
2019-05-06 23:33:16 +0200

    Posat consulta encaixa fixa fixe

--- src/Fitxa.java
+++ src/Fitxa.java
@@ -7,14 +7,8 @@ public class Fitxa extends Excepcio{
     private ArrayList<Regio> regions;
     private Posicio pos;
 
-    //Pre:---
-    //Post:copia la fitxa f a l'actual
-    public Fitxa(Fitxa f){
-        regions = new ArrayList<Regio>(5);
-        for (int i = 0; i < f.getRegions().size(); i++) {
-            regions.add(i,f.getRegions().get(i));
-        }
-        pos.setPosicio(f.getPosicio().getPosicioX(),f.getPosicio().getPosicioY());
+    public Fitxa(){
+        pos=null;
     }
 
     //Pre: lletres mida = 5
@@ -27,17 +21,10 @@ public class Fitxa extends Excepcio{
                 Regio nou = new Regio(lletra);
                 regions.add(nou);
             }
-            pos.setRotacio(0);
         }
         else {
             throw new Excepcio("La mida és incorrecta");
         }
-    }
-
-    //Pre:---
-    //Post: retorna la llista de regions de la fitxa
-    public ArrayList<Regio> getRegions(){
-        return regions;
     }
 
     //Pre: regio = C, N, E, S o O
@@ -143,48 +130,48 @@ public class Fitxa extends Excepcio{
     }
 
     //Pre: rotar = 90 o 180 o 270
-    //Post: retorna una fitxa rotada
-    public Fitxa rotar_fitxa(int rotar)throws Excepcio{
-
-        Fitxa nou;
-        if(rotar == 90 || rotar == 180 || rotar == 270) {
-            nou = new Fitxa(this);
-            nou.pos.setRotacio(rotar);
-            Regio aux;
-            if (rotar == 90) {
-                aux = new Regio(regio_n());
-                for (int i = 1; i < 4; i++) {
-                    nou.getRegions().remove(i);
-                    nou.getRegions().add(i, nou.getRegions().get(i + 1));
-                }
-                nou.getRegions().remove(4);
-                nou.getRegions().add(4, aux);
-            } else if (rotar == 180) {
-                aux = new Regio(regio_n());
-                nou.getRegions().remove(1);
-                nou.getRegions().add(1, nou.getRegions().get(3));
-                nou.getRegions().remove(3);
-                nou.getRegions().add(3, aux);
-
-                aux = new Regio(regio_e());
-                nou.getRegions().remove(2);
-                nou.getRegions().add(2, nou.getRegions().get(4));
-                nou.getRegions().remove(4);
-                nou.getRegions().add(4, aux);
-            } else {
-                aux = new Regio(regio_o());
-                for (int i = 4; i > 1; i--) {
-                    nou.getRegions().remove(i);
-                    nou.getRegions().add(i, nou.getRegions().get(i - 1));
-                }
-                nou.getRegions().remove(1);
-                nou.getRegions().add(1, aux);
+    //Post: fitxa rotat
+    public void rotar(int rotar){
+        Regio aux;
+        if(rotar==90){
+            aux = new Regio(regio_n());
+            for(int i=1; i<4; i++){
+                regions.remove(i);
+                regions.add(i,regions.get(i+1));
             }
+            regions.remove(4);
+            regions.add(4,aux);
+        }
+        else if(rotar==180){
+            aux = new Regio(regio_n());
+            regions.remove(1);
+            regions.add(1,regions.get(3));
+            regions.remove(3);
+            regions.add(3,aux);
+            aux = null;
+
+            aux = new Regio(regio_e());
+            regions.remove(2);
+            regions.add(2,regions.get(4));
+            regions.remove(4);
+            regions.add(4,aux);
+            aux = null;
+        }
+        else if(rotar==270){
+            aux = new Regio(regio_o());
+            for(int i=4; i>1; i--){
+                regions.remove(i);
+                regions.add(i,regions.get(i-1));
+            }
+            regions.remove(1);
+            regions.add(1,aux);
         }
         else{
-            throw new Excepcio("Rotació incorrecta");
+            Gui.print("Rotacio de fitxa incorrecte");
         }
-        return nou;
+        int rotacio=pos.getRotacio()+rotar;
+        if(rotacio>=360)rotacio-=360;
+        pos.setRotacio(rotacio);
     }
 
     //Pre:---
@@ -210,31 +197,36 @@ public class Fitxa extends Excepcio{
 
     //Pre:---
     //Post:retorna cert si la fitxa actual encaixa amb la fitxa 'f' en la posicio del costat de 'direccio' ('N','E','S' o 'O')
-    public boolean fitxaActualEncaixaAmb(Fitxa f , char direccio) throws Excepcio{
+    public boolean fitxaActualEncaixaAmb(Fitxa f , char direccio){
+        rotar(pos.getRotacio());
+        f.rotar(f.getPosicio().getRotacio());
+        boolean encaixa=false;
         if(direccio == 'N'){
             if(f.regio_n()==this.regio_s()){
-                return true;
+                encaixa= true;
             }
         }
         else if(direccio == 'E'){
             if(f.regio_e()==this.regio_o()){
-                return true;
+                encaixa= true;
             }
         }
         else if(direccio == 'S'){
             if(f.regio_s()==this.regio_n()){
-                return true;
+                encaixa= true;
             }
         }
         else if(direccio == 'O'){
             if(f.regio_o()==this.regio_e()){
-                return true;
+                encaixa= true;
             }
         }
         else{
-            throw new Excepcio("Direcció incorrecta");
+            Gui.print("Posició de fitxa incorrecte");
         }
-        return false;
+        f.rotar(360-f.getPosicio().getRotacio());
+        rotar(360-pos.getRotacio());
+        return encaixa;
     }
 
 
