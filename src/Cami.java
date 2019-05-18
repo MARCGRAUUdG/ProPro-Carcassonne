928f7e6184f1575dae979612d255847230d26b45
Teng <tengliuchen@hotmail.com>
2019-05-18 16:16:52 +0200

    Possessio propietari canviat compta el final

--- src/Cami.java
+++ src/Cami.java
@@ -40,6 +40,31 @@ public class Cami extends Estructura {
     }
 
     //Pre:---
+    //Post:retorna el/s propietari/s del cami altrament llista buida
+    public List<Integer> propietari(){
+        List<Integer> pro = new ArrayList<Integer>(Arrays.asList(0,0,0,0));
+        for (int i =0; i<getConjunt().size(); i++){
+            if(getConjunt().get(i).getKey().regio_c()=='C' && getConjunt().get(i).getKey().regio_c_seguidor()>0){
+                pro.set(getConjunt().get(i).getKey().regio_c_seguidor()-1,pro.get(getConjunt().get(i).getKey().regio_c_seguidor()-1)+1);
+            }
+            if(getConjunt().get(i).getKey().regio_n()=='C' && getConjunt().get(i).getKey().regio_n_seguidor()>0){
+                pro.set(getConjunt().get(i).getKey().regio_n_seguidor()-1,pro.get(getConjunt().get(i).getKey().regio_n_seguidor()-1)+1);
+            }
+            if(getConjunt().get(i).getKey().regio_e()=='C' && getConjunt().get(i).getKey().regio_e_seguidor()>0){
+                pro.set(getConjunt().get(i).getKey().regio_e_seguidor()-1,pro.get(getConjunt().get(i).getKey().regio_e_seguidor()-1)+1);
+            }
+            if(getConjunt().get(i).getKey().regio_s()=='C' && getConjunt().get(i).getKey().regio_s_seguidor()>0){
+                pro.set(getConjunt().get(i).getKey().regio_s_seguidor()-1,pro.get(getConjunt().get(i).getKey().regio_s_seguidor()-1)+1);
+            }
+            if(getConjunt().get(i).getKey().regio_o()=='C' && getConjunt().get(i).getKey().regio_o_seguidor()>0){
+                pro.set(getConjunt().get(i).getKey().regio_o_seguidor()-1,pro.get(getConjunt().get(i).getKey().regio_o_seguidor()-1)+1);
+            }
+        }
+
+        return llistaPropietari(pro);
+    }
+
+    //Pre:---
     //Post:retorna punts total del cami
     public int punts(){
         return getConjunt().size();
