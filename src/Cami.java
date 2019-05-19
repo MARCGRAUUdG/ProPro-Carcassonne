8a778461bea0c1414f298cd2ed99c43de393161b
Teng <tengliuchen@hotmail.com>
2019-05-19 15:52:15 +0200

    cami i creuament tractament diferent fet (bug arreglat)

--- src/Cami.java
+++ src/Cami.java
@@ -44,20 +44,37 @@ public class Cami extends Estructura {
     public List<Integer> propietari(){
         List<Integer> pro = new ArrayList<Integer>(Arrays.asList(0,0,0,0));
         for (int i =0; i<getConjunt().size(); i++){
-            if(getConjunt().get(i).getKey().regio_c()=='C' && getConjunt().get(i).getKey().regio_c_seguidor()>0){
-                pro.set(getConjunt().get(i).getKey().regio_c_seguidor()-1,pro.get(getConjunt().get(i).getKey().regio_c_seguidor()-1)+1);
+            if (getConjunt().get(i).getKey().regio_c()=='X'){
+                char r = getConjunt().get(i).getValue().get(0);
+                if (r == 'N' && getConjunt().get(i).getKey().regio_n_seguidor() > 0) {
+                    pro.set(getConjunt().get(i).getKey().regio_n_seguidor() - 1, pro.get(getConjunt().get(i).getKey().regio_n_seguidor() - 1) + 1);
+                }
+                if(r == 'E' && getConjunt().get(i).getKey().regio_e_seguidor() > 0){
+                    pro.set(getConjunt().get(i).getKey().regio_e_seguidor() - 1, pro.get(getConjunt().get(i).getKey().regio_e_seguidor() - 1) + 1);
+                }
+                if (r == 'S' && getConjunt().get(i).getKey().regio_s_seguidor() > 0) {
+                    pro.set(getConjunt().get(i).getKey().regio_s_seguidor() - 1, pro.get(getConjunt().get(i).getKey().regio_s_seguidor() - 1) + 1);
+                }
+                if (r == 'O' && getConjunt().get(i).getKey().regio_o_seguidor() > 0) {
+                    pro.set(getConjunt().get(i).getKey().regio_o_seguidor() - 1, pro.get(getConjunt().get(i).getKey().regio_o_seguidor() - 1) + 1);
+                }
             }
-            if(getConjunt().get(i).getKey().regio_n()=='C' && getConjunt().get(i).getKey().regio_n_seguidor()>0){
-                pro.set(getConjunt().get(i).getKey().regio_n_seguidor()-1,pro.get(getConjunt().get(i).getKey().regio_n_seguidor()-1)+1);
-            }
-            if(getConjunt().get(i).getKey().regio_e()=='C' && getConjunt().get(i).getKey().regio_e_seguidor()>0){
-                pro.set(getConjunt().get(i).getKey().regio_e_seguidor()-1,pro.get(getConjunt().get(i).getKey().regio_e_seguidor()-1)+1);
-            }
-            if(getConjunt().get(i).getKey().regio_s()=='C' && getConjunt().get(i).getKey().regio_s_seguidor()>0){
-                pro.set(getConjunt().get(i).getKey().regio_s_seguidor()-1,pro.get(getConjunt().get(i).getKey().regio_s_seguidor()-1)+1);
-            }
-            if(getConjunt().get(i).getKey().regio_o()=='C' && getConjunt().get(i).getKey().regio_o_seguidor()>0){
-                pro.set(getConjunt().get(i).getKey().regio_o_seguidor()-1,pro.get(getConjunt().get(i).getKey().regio_o_seguidor()-1)+1);
+            else {
+                if (getConjunt().get(i).getKey().regio_c() == 'C' && getConjunt().get(i).getKey().regio_c_seguidor() > 0) {
+                    pro.set(getConjunt().get(i).getKey().regio_c_seguidor() - 1, pro.get(getConjunt().get(i).getKey().regio_c_seguidor() - 1) + 1);
+                }
+                if (getConjunt().get(i).getKey().regio_n() == 'C' && getConjunt().get(i).getKey().regio_n_seguidor() > 0) {
+                    pro.set(getConjunt().get(i).getKey().regio_n_seguidor() - 1, pro.get(getConjunt().get(i).getKey().regio_n_seguidor() - 1) + 1);
+                }
+                if (getConjunt().get(i).getKey().regio_e() == 'C' && getConjunt().get(i).getKey().regio_e_seguidor() > 0) {
+                    pro.set(getConjunt().get(i).getKey().regio_e_seguidor() - 1, pro.get(getConjunt().get(i).getKey().regio_e_seguidor() - 1) + 1);
+                }
+                if (getConjunt().get(i).getKey().regio_s() == 'C' && getConjunt().get(i).getKey().regio_s_seguidor() > 0) {
+                    pro.set(getConjunt().get(i).getKey().regio_s_seguidor() - 1, pro.get(getConjunt().get(i).getKey().regio_s_seguidor() - 1) + 1);
+                }
+                if (getConjunt().get(i).getKey().regio_o() == 'C' && getConjunt().get(i).getKey().regio_o_seguidor() > 0) {
+                    pro.set(getConjunt().get(i).getKey().regio_o_seguidor() - 1, pro.get(getConjunt().get(i).getKey().regio_o_seguidor() - 1) + 1);
+                }
             }
         }
 
